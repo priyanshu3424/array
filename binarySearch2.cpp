c19aec45db9d087abb4af514c645af7c3508ef19
priyanshu3424 <140826461+priyanshu3424@users.noreply.github.com>
2024-04-02 14:15:41 +0530

    Update binarySearch2.cpp

--- binarySearch2.cpp
+++ binarySearch2.cpp
@@ -6,7 +6,7 @@ int binarySearch(int arr[], int size, int key){
     int start=0;
     int end=size-1;
 
-    int mid=(start+end)/2;
+    int mid=start +(end-start)/2;
 
     while (start<=end){
 
@@ -19,19 +19,21 @@ int binarySearch(int arr[], int size, int key){
         else{
             end=mid-1;
         }
-        mid=(start+end)/2;
+        mid=start +(end-start)/2;
     }
     return -1;
     
 }
 int main(){
 
-    int even[6]={2,4,6,8,12,18};
     int odd[5]={3,8,11,14,16};
+    int even[6]={2,4,6,8,12,18};
+   
+    int oddindex= binarySearch(odd,5,8);
+    cout<<"Index of 8 is "<< oddindex <<endl;
 
-    int index= binarySearch(odd,5,3);
-
-    cout<<"Index of 3 is "<<index<<endl;
+    int evenindex= binarySearch(even,6,2);
+    cout<<"Index of 2 is "<< evenindex <<endl;
 
     return 0;
 }
