10965dc20bcf43de4f3eb2dfda09f9164940dcd6
priyanshu3424 <140826461+priyanshu3424@users.noreply.github.com>
2024-04-05 15:25:09 +0530

    Update bubblesort.cpp

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -1,22 +1,16 @@
-// C++ program for implementation 
-// of Bubble sort 
 #include <bits/stdc++.h> 
 using namespace std; 
 
-// A function to implement bubble sort 
 void bubbleSort(int arr[], int n) 
 { 
 	int i, j; 
 	for (i = 0; i < n - 1; i++) 
 
-		// Last i elements are already 
-		// in place 
 		for (j = 0; j < n - i - 1; j++) 
 			if (arr[j] > arr[j + 1]) 
 				swap(arr[j], arr[j + 1]); 
 } 
 
-// Function to print an array 
 void printArray(int arr[], int size) 
 { 
 	int i; 
@@ -25,7 +19,6 @@ void printArray(int arr[], int size)
 	cout << endl; 
 } 
 
-// Driver code 
 int main() 
 { 
 	int arr[] = { 5, 1, 4, 2, 8}; 
