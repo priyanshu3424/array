087608dafb36faa8d661d5e8d14d4769a2d3592e
priyanshu3424 <140826461+priyanshu3424@users.noreply.github.com>
2024-04-06 23:23:18 +0530

    Update swapalternate.cpp

--- swapalternate.cpp
+++ swapalternate.cpp
@@ -2,14 +2,12 @@
 using namespace std;
 
 void printArray(int arr[], int n){
-
     for(int i=0;i<n;i++){
        cout<<arr[i] <<" ";
     }cout<<endl;
 }
 
 void swapAlternate(int arr[], int size){
-
     for(int i=0;i<size;i+=2){
         if(i+1 < size){
             swap (arr[i], arr[i+1]);
