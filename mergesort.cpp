2b5d86135a5d6cf79110f37266610b99ab8b492b
priyanshu3424 <140826461+priyanshu3424@users.noreply.github.com>
2024-04-09 23:16:35 +0530

    Update mergesort.cpp

--- mergesort.cpp
+++ mergesort.cpp
@@ -1,21 +1,15 @@
-// C++ program for Merge Sort
 #include <bits/stdc++.h>
 using namespace std;
 
-// Merges two subarrays of array[].
-// First subarray is arr[begin..mid]
-// Second subarray is arr[mid+1..end]
 void merge(int array[], int const left, int const mid,
 		int const right)
 {
 	int const subArrayOne = mid - left + 1;
 	int const subArrayTwo = right - mid;
 
-	// Create temp arrays
 	auto *leftArray = new int[subArrayOne],
 		*rightArray = new int[subArrayTwo];
 
-	// Copy data to temp arrays leftArray[] and rightArray[]
 	for (auto i = 0; i < subArrayOne; i++)
 		leftArray[i] = array[left + i];
 	for (auto j = 0; j < subArrayTwo; j++)
@@ -24,7 +18,6 @@ void merge(int array[], int const left, int const mid,
 	auto indexOfSubArrayOne = 0, indexOfSubArrayTwo = 0;
 	int indexOfMergedArray = left;
 
-	// Merge the temp arrays back into array[left..right]
 	while (indexOfSubArrayOne < subArrayOne
 		&& indexOfSubArrayTwo < subArrayTwo) {
 		if (leftArray[indexOfSubArrayOne]
@@ -41,8 +34,6 @@ void merge(int array[], int const left, int const mid,
 		indexOfMergedArray++;
 	}
 
-	// Copy the remaining elements of
-	// left[], if there are any
 	while (indexOfSubArrayOne < subArrayOne) {
 		array[indexOfMergedArray]
 			= leftArray[indexOfSubArrayOne];
@@ -50,8 +41,6 @@ void merge(int array[], int const left, int const mid,
 		indexOfMergedArray++;
 	}
 
-	// Copy the remaining elements of
-	// right[], if there are any
 	while (indexOfSubArrayTwo < subArrayTwo) {
 		array[indexOfMergedArray]
 			= rightArray[indexOfSubArrayTwo];
@@ -62,8 +51,6 @@ void merge(int array[], int const left, int const mid,
 	delete[] rightArray;
 }
 
-// begin is for left index and end is right index
-// of the sub-array of arr to be sorted
 void mergeSort(int array[], int const begin, int const end)
 {
 	if (begin >= end)
@@ -75,16 +62,12 @@ void mergeSort(int array[], int const begin, int const end)
 	merge(array, begin, mid, end);
 }
 
-// UTILITY FUNCTIONS
-// Function to print an array
 void printArray(int A[], int size)
 {
 	for (int i = 0; i < size; i++)
 		cout << A[i] << " ";
 	cout << endl;
 }
-
-// Driver code
 int main()
 {
 	int arr[] = { 12, 11, 13, 5, 6, 7 };
@@ -99,6 +82,3 @@ int main()
 	printArray(arr, arr_size);
 	return 0;
 }
-
-// This code is contributed by Mayank Tyagi
-// This code was revised by Joshua Estes
