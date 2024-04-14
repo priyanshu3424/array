19212d8253a599678c7a2ec4f32478ac412955cd
priyanshu3424 <140826461+priyanshu3424@users.noreply.github.com>
2024-04-14 22:51:27 +0530

    Add files via upload

---
+++ char2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    char name[20];
+
+    cout<<"Enter your name"<<endl;
+    cin>>name;
+    name[2]='\0';
+
+    cout <<"Your name is ";
+    cout<<name<<endl;
+
+    return 0;
+}
