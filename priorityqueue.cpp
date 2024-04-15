ba5f46e7d2b31c028226a401978717a4ddaebedc
priyanshu3424 <140826461+priyanshu3424@users.noreply.github.com>
2024-04-15 23:02:34 +0530

    Update priorityqueue.cpp

--- priorityqueue.cpp
+++ priorityqueue.cpp
@@ -2,10 +2,7 @@
 #include<queue>
 using namespace std;
 int main(){
-    //max heap
     priority_queue<int> maxi;
-
-    //min heap
     priority_queue<int,vector<int>,greater<int> >mini;
 
     maxi.push(1);
