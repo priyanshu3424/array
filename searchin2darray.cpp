2bcbff4d64740ef39e1648400e894ef2a9205626
priyanshu3424 <140826461+priyanshu3424@users.noreply.github.com>
2024-04-17 23:12:01 +0530

    Add files via upload

---
+++ searchin2darray.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+bool isPresent(int arr[3][4], int target, int row, int col){
+
+    for(int row=0;row<3;row++){
+        for (int col=0;col<4;col++){
+
+            if (arr[row][col]==target){
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+int main(){
+
+    //create 2d array
+    int arr[3][4];
+
+     // taking input --> row wise input
+    for(int row= 0; row< 4; row++) {
+        for(int col = 0; col < 3; col++) {
+            cin >> arr[row][col];
+        }
+    }
+
+    //print
+     for(int row= 0; row< 3; row++) {
+        for(int col= 0; col< 4; col++) {
+            cout << arr[row][col] << " ";
+        }
+        cout << endl;
+    }
+
+    cout<<"Enter the element to search " <<endl;
+    int target;
+    cin>>target;
+
+    if(isPresent(arr,target,3,4)){
+        cout<<"Element found"<<endl;
+    }
+    else{
+        cout<<"Not found"<<endl;
+    }
+    return 0;
+
+}
