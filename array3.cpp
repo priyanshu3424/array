9dc37ccf6970d846156a4238b7c2b3c70073c3cc
Priyanshu Jain <140826461+priyanshu3424@users.noreply.github.com>
2024-04-30 23:55:39 +0530

    Update array3.cpp

--- array3.cpp
+++ array3.cpp
@@ -6,14 +6,11 @@ void printarray(){
 
 int main(){
 
-    //declare
     int third[15] = {3,7};
 
     int n=15;
 
-    //accessing an array
     cout<<endl <<"Printing the array"<<endl;
-    //print the array
     
     for(int i=0;i<n;i++){
         cout<<third[i]<<" ";
